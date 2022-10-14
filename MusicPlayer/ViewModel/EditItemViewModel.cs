ed7158173a525e3ec594435bce695f5138fd5efe
motifoye <goaway0137@gmail.com>
2022-10-14 14:38:31 +0700

    back14-10

--- MusicPlayer/ViewModel/EditItemViewModel.cs
+++ MusicPlayer/ViewModel/EditItemViewModel.cs
@@ -1,16 +1,6 @@
 ﻿using DevExpress.Mvvm;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using VKApplication.Model;
-//using VKApplication.App.Model;
 
 namespace VKApplication.App.ViewModel
 {
